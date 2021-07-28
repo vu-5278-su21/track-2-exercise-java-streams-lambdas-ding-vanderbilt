0d78821db53318000b4512c76dea644e75c2ec40
Guoliang Ding <Guoliang.Ding@vanderbilt.edu>
2021-07-28 12:23:23 -0400

    track2 final exercise

--- src/main/java/edu/vanderbilt/cs/streams/BikeRide.java
+++ src/main/java/edu/vanderbilt/cs/streams/BikeRide.java
@@ -125,7 +125,7 @@ public class BikeRide {
     // Hint: see Arrays.stream(...)
     //
     public DoubleStream heartRateStream() {
-        return DoubleStream.empty();
+        return Arrays.stream(heartRate);
     }
 
     // @ToDo:
@@ -134,7 +134,7 @@ public class BikeRide {
     // stream of the specified values
     //
     public DoubleStream velocityStream() {
-        return DoubleStream.empty();
+        return Arrays.stream(velocity);
     }
 
     // @ToDo:
@@ -142,7 +142,7 @@ public class BikeRide {
     // Implement this method so it returns a
     // stream of the specified values
     public DoubleStream gradeStream() {
-        return DoubleStream.empty();
+        return Arrays.stream(grade);
     }
 
     // @ToDo:
@@ -150,7 +150,7 @@ public class BikeRide {
     // Implement this method so it returns a
     // stream of the specified values
     public DoubleStream altitudeStream() {
-        return DoubleStream.empty();
+        return Arrays.stream(altitude);
     }
 
     // @ToDo:
@@ -158,7 +158,7 @@ public class BikeRide {
     // Implement this method so it returns a
     // stream of the specified values
     public Stream<LatLng> coordinateStream() {
-        return Stream.empty();
+        return Arrays.stream(coordinates);
     }
 
 
@@ -171,7 +171,17 @@ public class BikeRide {
     // data arrays (e.g., heartRate, velocity, etc.)
     //
     public Stream<DataFrame> fusedFramesStream() {
-        return Stream.empty();
+    	
+        return IntStream.range(0, coordinates.length)
+                		.mapToObj(index -> new DataFrame(
+					                            coordinates[index],
+					                            grade[index],
+					                            altitude[index],
+					                            velocity[index],
+					                            heartRate[index]
+					                    )
+                );
+
     }
 
 
